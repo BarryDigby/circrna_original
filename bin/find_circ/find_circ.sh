fb72875ff704c8b91f5b09e0f28e81ae108386a3
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-09-07 18:23:46 +0100

    Update find_circ.sh

--- bin/find_circ/find_circ.sh
+++ bin/find_circ/find_circ.sh
@@ -16,10 +16,10 @@ base=$(echo $R1 | cut -f 1 -d'.')
 
         samtools view -hf 4 ${base}.bam | samtools view -Sb - > ${base}_unmapped.bam
 
-        find_circ/unmapped2anchors.py ${base}_unmapped.bam | gzip > ${base}_anchors.qfa.gz
+        unmapped2anchors.py ${base}_unmapped.bam | gzip > ${base}_anchors.qfa.gz
 
         bowtie2 -p 8 --reorder --mm -D 20 --score-min=C,-15,0 -q -x $index \
-        -U ${base}_anchors.qfa.gz | find_circ/find_circ.py -G $genome -p ${base} -s ${base}.sites.log > ${base}.sites.bed 2> ${base}.sites.reads
+        -U ${base}_anchors.qfa.gz | find_circ.py -G $genome -p ${base} -s ${base}.sites.log > ${base}.sites.bed 2> ${base}.sites.reads
 
         echo "#chrom:start:end:name:n_reads:strand:n_uniq:best_qual_A:best_qual_B:spliced_at_begin:spliced_at_end:tissues:tiss_counts:edits:anchor_overlap:breakpoints" > $base/tmp
 
@@ -27,7 +27,7 @@ base=$(echo $R1 | cut -f 1 -d'.')
 
         rm $base/tmp
 
-        grep circ ${base}.sites.bed | grep -v chrM | find_circ/sum.py -2,3 | find_circ/scorethresh.py -16 1 | find_circ/scorethresh.py -15 2 | find_circ/scorethresh.py -14 2 | find_circ/scorethresh.py 7 2 | find_circ/scorethresh.py 8,9 35 | find_circ/scorethresh.py -17 100000 >> ${base}.circ_candidates.bed
+        grep circ ${base}.sites.bed | grep -v chrM | sum.py -2,3 | scorethresh.py -16 1 | scorethresh.py -15 2 | scorethresh.py -14 2 | scorethresh.py 7 2 | scorethresh.py 8,9 35 | scorethresh.py -17 100000 >> ${base}.circ_candidates.bed
 
         rm ${base}.bam
         rm ${base}_anchors_unmapped.bam
