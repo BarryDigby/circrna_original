8e67677258dc57142097a9ef8f4cb65cd349c5f6
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-09-14 20:21:02 +0100

    Update filter_CIRIquant.sh

--- bin/filter_CIRIquant.sh
+++ bin/filter_CIRIquant.sh
@@ -5,7 +5,7 @@ base=$(basename $input .gtf)
 
 ## strip headers from file and remove BSJ with less than 2 reads
 grep -v "#" $input | grep -v "bsj 1.000" > ${base}.filtered
-awk '{print \$14}' ${base}.filtered | cut -d'.' -f1 > counts
+awk '{print $14}' ${base}.filtered | cut -d'.' -f1 > counts
 cut -f 1,4,5,7 ${base}.filtered > ${base}.txt
 paste ${base}.txt counts > ${base}.bed.tmp
 
