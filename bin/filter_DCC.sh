5478ce1132471213bcf2eb79ff2aab92cf5c1938
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-09-14 17:16:52 +0100

    Create filter_DCC.sh

---
+++ bin/filter_DCC.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+input=$1
+base=$(basename $input .txt)
+
+## fix start position (+1) compared to circexplorer2, find_circ, circRNA_finder
+while read line 
+        do
+
+        chr=$(echo $line | awk '{print $1}')
+        start=$(echo $line | awk '{sum = $2 -1; print sum}')
+        stop=$(echo $line | awk '{print $3}')
+        sign=$(echo $line | awk '{print $5}')
+        count=$(echo $line | awk '{print $4}')
+        
+        echo -e "$chr\t$start\t$stop\t$sign\t$count" >> ${base}.bed
+
+done < $input
