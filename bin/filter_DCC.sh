b8d33224b18264c5f35248990d7194710676b174
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-10-09 17:28:59 +0100

    Update filter_DCC.sh

--- bin/filter_DCC.sh
+++ bin/filter_DCC.sh
@@ -7,15 +7,4 @@ base=$(basename $input .txt)
 awk '{if($5 > 1) print $0}' $input > ${base}.filtered
 
 ## fix start position (+1) compared to circexplorer2, find_circ, circRNA_finder
-while read line 
-        do
-
-        chr=$(echo $line | awk '{print $1}')
-        start=$(echo $line | awk '{sum = $2 -1; print sum}')
-        stop=$(echo $line | awk '{print $3}')
-        sign=$(echo $line | awk '{print $4}')
-        count=$(echo $line | awk '{print $5}')
-        
-        echo -e "$chr\t$start\t$stop\t$sign\t$count" >> ${base}.bed
-
-done < ${base}.filtered
+awk -v OFS="\t" '{$2-=1;print}' ${base}.filtered > ${base}.bed
