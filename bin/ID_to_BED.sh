26c843639075fffe852dc21a94f03bc2d8f753d1
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-10-14 15:16:54 +0100

    Update ID_to_BED.sh

--- bin/ID_to_BED.sh
+++ bin/ID_to_BED.sh
@@ -8,7 +8,7 @@ while IFS='' read -r line; do
 	chr=$(echo $line | cut -d: -f1)
 	start=$(echo $line | cut -d- -f1 | cut -d: -f2)
 	stop=$(echo $line | cut -d- -f2 | cut -d: -f1)
-	sign=$(echo $line | cut -d- -f2 | cut -d: -f2)
+	sign=$(echo $line | cut -d: -f3)
 
 	echo -e "$chr\t$start\t$stop\t$name\t0\t$sign" >> ${name}.bed
 
