6dc44c4323621220e8fd1faf68486dda01303822
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-10-21 11:16:44 +0100

    Update get_parent_genes.sh

--- bin/get_parent_genes.sh
+++ bin/get_parent_genes.sh
@@ -8,7 +8,6 @@ while IFS='' read -r line; do
         echo "$line" >> ${name}.bed_tmp
         sed 's/[\t]*$//' ${name}.bed_tmp > ${name}.bed && rm ${name}.bed_tmp
         gene=$(bedtools intersect -a ${name}.bed -b filt.gtf -s -f 1.00 -wb | awk -F'gene_name ' '{print $2}' | awk -F';' '{print $1}' | sed 's/"//g' | uniq | head -n 1)
-        echo $gene
         rm ${name}.bed
 
         echo -e "$name\t$gene" >> parent_genes/${name}.parent_genes.txt
