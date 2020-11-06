5bf81bf8510c0262203aab06e7a12122aa183c91
BarryDigby <58001749+BarryDigby@users.noreply.github.com>
2020-11-06 17:25:02 +0000

    Update annotate_report.R

--- bin/annotate_report.R
+++ bin/annotate_report.R
@@ -8,7 +8,8 @@ mart <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
 results <- getBM(attributes=c("entrezgene_description", "hgnc_symbol"), mart=mart)
 colnames(results) <- c("Description", "Parent_Gene")
 
-x <- merge(x, results, by="Parent_Gene")
+# allow for missing descriptions! Crucial to capture lncRNAs
+x <- merge(x, results, by="Parent_Gene", all.x=T)
 x <- subset(x, select=c(circRNA_ID, Parent_Gene, Description, Strand, Log2FC, pvalue, Adjusted_pvalue))
 
 write.table(x, "DE_circRNA_Report.txt", quote=F, sep="\t", row.names=F)
